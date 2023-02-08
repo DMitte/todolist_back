3cf9548861a9afe07d117900a43c00c7c23e3658
Dany Mitte <mittedany547@gmail.com>
2023-02-08 11:39:11 -0500

    autenticacion terminada

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) =>{
     const token = jwt.sign({
         name: user.name,
         id: user._id
-    },process.env.TOKEN_SECRET)
+    },process.env.TOKEN_SECRET,{expiresIn: '1d'})
 
     res.header('auth-token', token).json({
         error: null,
